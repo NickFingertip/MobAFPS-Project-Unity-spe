2eb3773a763842a85d1a2aeace57f249bd23248c
Wilko <w.willame@hotmail.fr>
2015-03-20 18:21:59 +0100

    CharacterController Commit

---
+++ CharacController/New Unity Project/Assets/Scripts/FireBallBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireBallBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnEnable(){
+		Invoke("AutoDisable", 5.0f);
+	}
+
+	void AutoDisable(){
+		gameObject.SetActive(false);
+	}
+}
